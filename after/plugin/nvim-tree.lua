fc3a7c178c297e2e27b6c0ad002200439d92663d
Josh Ellis <30234012+senatormirror@users.noreply.github.com>
2024-09-18 18:32:21 +0100

    Add nvim-tree configuration

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -3,4 +3,16 @@ vim.g.loaded_netrwPlugin = 1
 
 vim.opt.termguicolors = true
 
-require("nvim-tree").setup()
+local function on_attach(bufnr)
+	local api = require("nvim-tree.api")
+
+	local function opts(desc)
+		return( { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true } )
+	end
+
+	api.config.mappings.default_on_attach(bufnr)
+end
+
+require("nvim-tree").setup( {
+	on_attach = on_attach
+} )
